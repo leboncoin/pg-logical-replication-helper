9bc1ab99e072e57e58eb5dd843f031891da3cd76
Laurent Dutheil <ldutheil74@gmail.com>
2026-01-16 15:50:05 +0100

    parametrize postgresql version in docker-compose.yml to match locally installed pg_dump

--- replication_start_test.py
+++ replication_start_test.py
@@ -1,4 +1,6 @@
 import os
+import re
+import subprocess
 
 import psycopg
 import pytest
@@ -10,6 +12,10 @@ from replication_start import main
 
 @pytest.fixture(scope="module", autouse=True)
 def setup(request):
+    pg_dump_version = subprocess.check_output(["pg_dump", "--version"], text=True,stderr=None)
+    pg_dump_version = re.match("pg_dump.*(\d{2})\..*", pg_dump_version).group(1)
+    os.environ["POSTGRES_VERSION"]= pg_dump_version
+
     compose = DockerCompose(
         context=".",
         compose_file_name="docker-compose.yml",
