c1cda08607aefecb49c382c3e74af0aa08ccb740
Laurent Dutheil <ldutheil74@gmail.com>
2026-01-23 15:26:31 +0100

    matrix build with PG versions from 14 to 18

--- replication_start_test.py
+++ replication_start_test.py
@@ -17,7 +17,11 @@ FAKE_TIME = datetime.datetime(2026, 1, 22, 10, 1, 25)
 
 @pytest.fixture(scope="module", autouse=True)
 def setup(request):
-    os.environ["POSTGRES_VERSION"] = detect_pg_dump_version()
+    if "POSTGRES_ORIGIN_VERSION" not in os.environ:
+        os.environ["POSTGRES_ORIGIN_VERSION"] = detect_pg_dump_version()
+
+    if "POSTGRES_DESTINATION_VERSION" not in os.environ:
+        os.environ["POSTGRES_DESTINATION_VERSION"] = detect_pg_dump_version()
 
     compose = DockerCompose(
         context=".",
